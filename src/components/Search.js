df6cf97c44625a8aa90408340df63ae4204c36ad
Eliza Plowden <elizaplowden@gmail.com>
2021-04-27 11:00:54 +0100

    added term to search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Search = () => {
   // hook instead of class component
   const [term, setTerm] = useState('');
+
+
+  useEffect(() => {
+    const search = async () => {
+      await axios.get('https://en.wikipedia.org/w/api/php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsarch: term,
+        }
+      });
+    };
+
+    search();
+  }, [term]);
 
   return (
     <div>
