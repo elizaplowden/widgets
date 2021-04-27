d4e317135b2d6bd79006774a71885e4cca2a7eb4
Eliza Plowden <elizaplowden@gmail.com>
2021-04-27 11:24:15 +0100

    linking to wikipedia page

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,38 +2,44 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Search = () => {
-  // hook instead of class component
   const [term, setTerm] = useState('programming');
   const [results, setResults] = useState([]);
 
-
   useEffect(() => {
     const search = async () => {
-      const { data } = await axios.get('https://en.wikipedia.org/w/api/php', {
+      const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
         params: {
           action: 'query',
           list: 'search',
           origin: '*',
           format: 'json',
-          srsarch: term,
+          srsearch: term,
         },
       });
 
-      setResults(data.query.search)
+      setResults(data.query.search);
     };
-
 
     search();
   }, [term]);
 
+
   const renderedResults = results.map((result) => {
     return (
-      <div className="item">
+      <div key={result.pageid} className="item">
+      <div className="right floated content">
+        <a
+          className="ui button"
+          href={`https://en.wikipedia.org?curid=${result.pageid}`}
+        >
+          Go
+        </a>
+      </div>
         <div className="content">
           <div className="header">
             {result.title}
           </div>
-          {result.snippet}
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
         </div>
       </div>
     );
