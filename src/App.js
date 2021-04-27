4fab1cb47366c45aa06cf9223d9bd28e93ca3d10
Eliza Plowden <elizaplowden@gmail.com>
2021-04-27 09:42:39 +0100

    onClick helper

--- src/App.js
+++ src/App.js
@@ -21,5 +21,5 @@ export default () => {
     <div>
       <Accordion items={items} />
     </div>
-    );
+  );
 };
--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,16 +1,24 @@
 import React from 'react';
 
 const Accordion = ({ items }) => {
-  const renderedItems = items.map(item => {
-    return <div key={item.title}>
-      <div className="title active">
+  const onTitleClick = (index) => {
+    console.log('Title clicked', index);
+  };
+
+  const renderedItems = items.map((item, index) => {
+    return <React.Fragment key={item.title}>
+      <div
+        className="title active"
+        // need the arrow function so we can run the function in the future
+        onClick={() => onTitleClick(index)}
+      >
         <i className="dropdown icon"></i>
         {item.title}
       </div>
       <div className="content active">
         <p>{item.content}</p>
       </div>
-    </div>
+    </React.Fragment>
   });
 
   return <div className="ui styled accordion">
