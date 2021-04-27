abfb8edb980ab716d140af799fff5883b4c3c1f5
Eliza Plowden <elizaplowden@gmail.com>
2021-04-27 13:31:22 +0100

    translate component

--- src/App.js
+++ src/App.js
@@ -32,21 +32,14 @@ const options = [
 ];
 
 const App = () => {
-  const [selected, setSelected] = useState(options[0]);
-  const [showDropdown, setShowDropdown] = useState(true);
+  // const [selected, setSelected] = useState(options[0]);
+  // const [showDropdown, setShowDropdown] = useState(true);
 
   return (
     <div>
-      <button onClick={() => setShowDropdown(!showDropdown)}>Toggle Dropdown</button>
-      {showDropdown ? (
-      <Dropdown
-        selected={selected}
-        onSelectedChange={setSelected}
-        options={options}
-      />
-      ) : null}
+      <Translate />
     </div>
   );
-};
+}
 
 export default App;
